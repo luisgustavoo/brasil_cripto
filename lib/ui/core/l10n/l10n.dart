d2ee4c30dd4049eec09219909a5375f88a938192
Luis Gustavo <luisgustavodevbr@gmail.com>
2025-09-10 18:11:39 -0300

    Adds internationalization (i18n) support
    Adds internationalization support to the application.
    
    This commit introduces:
    - The `l10n.yaml` configuration file for managing localization.
    - ARB files for English and Portuguese localizations.
    - Generated `app_localizations.dart` and related files for accessing localized strings.
    - An extension on `BuildContext` for easy access to localizations.

---
+++ lib/ui/core/l10n/l10n.dart
@@ -0,0 +1,6 @@
+import 'package:brasil_cripto/ui/core/l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+
+extension AppLocalizationsX on BuildContext {
+  AppLocalizations get l10n => AppLocalizations.of(this)!;
+}
