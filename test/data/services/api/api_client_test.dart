e4a9f6dba87744f4285b36c3241c2d581cc2b856
Luis Gustavo <luisgustavodevbr@gmail.com>
2025-09-12 13:40:54 -0300

    Work in progress

--- test/data/services/api/api_client_test.dart
+++ test/data/services/api/api_client_test.dart
@@ -9,20 +9,21 @@ import '../../../../testing/mocks.dart';
 import '../../../../testing/utils/result.dart';
 
 void main() {
+  late MockHttpClient mockHttpClient;
+  late ApiClient apiClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    apiClient = ApiClient(httpClient: mockHttpClient);
+  });
+
   group('ApiClient fetchCoinsMarkets', () {
-    late MockHttpClient mockHttpClient;
-    late ApiClient apiClient;
     late List<dynamic> jsonCoinsMarketsResponse;
     final jsonCoinsMarketsData = FixtureReader.getJsonData(
       'fakes/services/fixture/coins_markets_response.json',
     );
     jsonCoinsMarketsResponse =
         jsonDecode(jsonCoinsMarketsData) as List<dynamic>;
-
-    setUp(() {
-      mockHttpClient = MockHttpClient();
-      apiClient = ApiClient(httpClient: mockHttpClient);
-    });
 
     test(
       'should return a non-empty list when coins markets API returns data',
@@ -57,6 +58,41 @@ void main() {
         );
 
         final result = await apiClient.fetchCoinsMarkets('Bitcoin', 'usd');
+        expect(result, result.asError);
+      },
+    );
+  });
+
+  group('ApiClient fetchCoinsMarketsDetails', () {
+    final jsonCoinsMarketsDetailsData = FixtureReader.getJsonData(
+      'fakes/services/fixture/coins_markets_details_response.json',
+    );
+    final jsonCoinsMarketsDetailsResponse =
+        jsonDecode(jsonCoinsMarketsDetailsData) as Map<String, dynamic>;
+
+    const id = 'bitcoin';
+    test(
+      'should return Ok when coins market details API request succeeds',
+      () async {
+        mockHttpClient.mockGet<Map<String, dynamic>>(
+          '/coins/$id/market_chart',
+          object: jsonCoinsMarketsDetailsResponse,
+        );
+
+        final result = await apiClient.fetchCoinsMarketsDetails(id, 'usd', 1);
+        expect(result, isA<Ok<void>>());
+      },
+    );
+
+    test(
+      'should return Error when coins market details API request fails',
+      () async {
+        mockHttpClient.mockGet<Map<String, dynamic>>(
+          '/coins/$id/market_chart',
+          showError: true,
+        );
+
+        final result = await apiClient.fetchCoinsMarketsDetails(id, 'usd', 1);
         expect(result, result.asError);
       },
     );
