e291ffb666039a5b879f538de6481f3287640883
Luis Gustavo <luisgustavodevbr@gmail.com>
2025-09-13 16:54:43 -0300

    Refactoring tests

--- test/ui/home/widgets/home_screen_test.dart
+++ test/ui/home/widgets/home_screen_test.dart
@@ -3,6 +3,7 @@ import 'package:brasil_cripto/data/repositories/coins_markets/coins_markets_repo
 import 'package:brasil_cripto/data/repositories/favorites/favorites_repository.dart';
 import 'package:brasil_cripto/data/services/local/local_data_service.dart';
 import 'package:brasil_cripto/ui/coins_markets/view_models/coins_markets_view_model.dart';
+import 'package:brasil_cripto/ui/coins_markets/widgets/coins_market_screen.dart';
 import 'package:brasil_cripto/ui/core/l10n/l10n.dart';
 import 'package:brasil_cripto/ui/favorites/view_models/favorite_view_model.dart';
 import 'package:brasil_cripto/ui/home/widgets/home_screen.dart';
@@ -71,6 +72,20 @@ void main() {
     await tester.pumpAndSettle();
   }
 
+  Future<void> searchItem(WidgetTester tester) async {
+    await loadHomeScreen(tester);
+    final marketTab = find.byKey(const Key(marketTabKey));
+    await tester.tap(marketTab);
+    await tester.pumpAndSettle();
+    final searchField = find.byKey(const Key(searchEditKey));
+    await tester.tap(searchField);
+    await tester.pumpAndSettle();
+    await tester.enterText(searchField, 'Bitcoin');
+    await tester.testTextInput.receiveAction(TextInputAction.done);
+    await tester.pump(const Duration(milliseconds: 100));
+    expect(find.text('(BTC)'), findsOneWidget);
+  }
+
   group('HomeScreen / CoinsMarketScreen tests', () {
     testWidgets('should display title and empty state', (tester) async {
       await loadHomeScreen(tester);
@@ -83,54 +98,29 @@ void main() {
       tester,
     ) async {
       await loadHomeScreen(tester);
-      final marketTabButtonKey = find.byKey(const Key('market-tab-button'));
-      await tester.tap(marketTabButtonKey);
+      final marketTab = find.byKey(const Key(marketTabKey));
+      await tester.tap(marketTab);
       await tester.pumpAndSettle();
-      expect(find.byKey(const Key('search-edit')), findsOneWidget);
+      expect(find.byKey(const Key(searchEditKey)), findsOneWidget);
     });
 
     testWidgets('should search for a coin and display result', (tester) async {
-      await loadHomeScreen(tester);
-      await tester.pump();
-      final marketTabButton = find.byKey(const Key('market-tab-button'));
-      await tester.tap(marketTabButton);
-      await tester.pumpAndSettle();
-      final searchField = find.byKey(const Key('search-edit'));
-      await tester.tap(searchField);
-      await tester.pumpAndSettle();
-      await tester.enterText(searchField, 'Bitcoin');
-      await tester.testTextInput.receiveAction(TextInputAction.done);
-      await tester.pump(const Duration(milliseconds: 100));
-      expect(find.widgetWithText(Card, 'Bitcoin'), findsOneWidget);
+      await searchItem(tester);
     });
 
     testWidgets('should search for Bitcoin and favorite it', (tester) async {
-      await loadHomeScreen(tester);
-      await tester.pump();
-      final marketTabButtonKey = find.byKey(const Key('market-tab-button'));
-      await tester.tap(marketTabButtonKey);
-      await tester.pumpAndSettle();
-      final searchField = find.byKey(const Key('search-edit'));
-      await tester.tap(searchField);
-      await tester.pumpAndSettle();
-      await tester.enterText(searchField, 'Bitcoin');
-      await tester.testTextInput.receiveAction(TextInputAction.done);
-      await tester.pump(const Duration(milliseconds: 100));
-      final bitcoinCard = find.widgetWithText(Card, 'Bitcoin');
-      final starIcon = find.descendant(
-        of: bitcoinCard,
+      await searchItem(tester);
+      // Favorita o item
+      final starBorderIcon = find.descendant(
+        of: find.byKey(const Key('favorite-icon-bitcoin')),
         matching: find.byIcon(Icons.star_border),
       );
-      await tester.tap(starIcon);
+      await tester.tap(starBorderIcon);
       await tester.pump(const Duration(milliseconds: 100));
-      expect(
-        find.descendant(of: bitcoinCard, matching: find.byIcon(Icons.star)),
-        findsOneWidget,
-      );
-      final marketTabButton = find.byKey(const Key('market-tab-button'));
-      await tester.tap(marketTabButton);
-      await tester.pump();
-      expect(find.widgetWithText(Card, 'Bitcoin'), findsOneWidget);
+      final favoriteTab = find.byKey(const Key(favoriteTabKey));
+      await tester.tap(favoriteTab);
+      await tester.pump(const Duration(milliseconds: 100));
+      expect(find.text('(BTC)'), findsOneWidget);
     });
     testWidgets('should navigate to coin details when tapping on a coin card', (
       tester,
@@ -138,17 +128,7 @@ void main() {
       when(
         () => goRouter.push(any<String>(), extra: any(named: 'extra')),
       ).thenAnswer((_) async => null);
-      await loadHomeScreen(tester);
-      await tester.pump();
-      final marketTabButtonKey = find.byKey(const Key('market-tab-button'));
-      await tester.tap(marketTabButtonKey);
-      await tester.pumpAndSettle();
-      final searchField = find.byKey(const Key('search-edit'));
-      await tester.tap(searchField);
-      await tester.pump();
-      await tester.enterText(searchField, 'Bitcoin');
-      await tester.testTextInput.receiveAction(TextInputAction.done);
-      await tester.pump(const Duration(milliseconds: 100));
+      await searchItem(tester);
       final bitcoinCard = find.widgetWithText(Card, 'Bitcoin');
       expect(bitcoinCard, findsOneWidget);
       await tester.tap(bitcoinCard);
