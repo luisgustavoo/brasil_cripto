31b20d597e688af74b0d54e2227bea1fa1303980
Luis Gustavo <luisgustavodevbr@gmail.com>
2025-09-17 01:15:01 -0300

    Work in progress

--- testing/fakes/services/api/fake_api_client.dart
+++ testing/fakes/services/api/fake_api_client.dart
@@ -1,56 +1,23 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:brasil_cripto/data/services/api/api_client.dart';
 import 'package:brasil_cripto/data/services/api/models/coins_markets_api_model.dart';
 import 'package:brasil_cripto/data/services/api/models/market_api_model.dart';
 import 'package:brasil_cripto/utils/result.dart';
+import 'package:injectable/injectable.dart';
 
-import '../../../fixture/fixture_reader.dart';
+import '../../../models/coin.dart';
+import '../../../models/market.dart';
 
+@Injectable(as: ApiClient, env: [Environment.test])
 class FakeApiClient implements ApiClient {
-  FakeApiClient() {
-    final jsonCoinsMarketsData = FixtureReader.getJsonData(
-      'fakes/services/fixture/coins_markets_response.json',
-    );
-    jsonCoinsMarketsResponse =
-        jsonDecode(jsonCoinsMarketsData) as List<dynamic>;
-    final jsonCoinsMarketsDetailsData = FixtureReader.getJsonData(
-      'fakes/services/fixture/coins_markets_details_response.json',
-    );
-    jsonCoinsMarketsResponse =
-        jsonDecode(jsonCoinsMarketsData) as List<dynamic>;
-    jsonCoinsMarketsDetailsResponse =
-        jsonDecode(jsonCoinsMarketsDetailsData) as Map<String, dynamic>;
-  }
-
-  late List<dynamic> jsonCoinsMarketsResponse;
-  late Map<String, dynamic> jsonCoinsMarketsDetailsResponse;
-  StreamController<List<CoinsMarketsApiModel>> _favoritesCoinsController =
-      StreamController<List<CoinsMarketsApiModel>>();
-  StreamController<List<CoinsMarketsApiModel>> _coinsMarketsController =
-      StreamController<List<CoinsMarketsApiModel>>();
-
-  @override
-  Stream<List<CoinsMarketsApiModel>> get coinsMarketsStream =>
-      _coinsMarketsController.stream;
-
   @override
   Future<Result<List<CoinsMarketsApiModel>>> fetchCoinsMarkets(
     String? names,
     String vsCurrency, {
     bool isFavorite = false,
   }) {
-    final coinsMarketsList = List<Map<String, dynamic>>.from(
-      jsonCoinsMarketsResponse,
-    );
-    final coinsMarkets = coinsMarketsList
-        .map(CoinsMarketsApiModel.fromJson)
-        .toList();
-    _coinsMarketsController
-      ..add(coinsMarkets)
-      ..close();
-    return Future.value(Result.ok(coinsMarkets));
+    return Future.value(Result.ok(kCoinsMarketsApiModel));
   }
 
   @override
@@ -59,50 +26,6 @@ class FakeApiClient implements ApiClient {
     String vsCurrency,
     int days,
   ) {
-    final marketApiModel = MarketApiModel.fromJson(
-      jsonCoinsMarketsDetailsResponse,
-    );
-    return Future.value(Result.ok(marketApiModel));
-  }
-
-  @override
-  Future<void> startBackGroundFetchCoinsMarkets({
-    required String vsCurrency,
-    String? names,
-  }) async {
-    if (_coinsMarketsController.isClosed) {
-      _coinsMarketsController = StreamController<List<CoinsMarketsApiModel>>();
-    }
-  }
-
-  @override
-  void stopBackGroundFetch() {
-    _coinsMarketsController.close();
-    _favoritesCoinsController.close();
-  }
-
-  @override
-  Stream<List<CoinsMarketsApiModel>> get favoriteCoinsMarketStream =>
-      _favoritesCoinsController.stream;
-
-  @override
-  Future<void> startBackGroundFetchFavoritesCoins({
-    required String vsCurrency,
-    String? names,
-  }) async {
-    if (_favoritesCoinsController.isClosed) {
-      _favoritesCoinsController =
-          StreamController<List<CoinsMarketsApiModel>>();
-    }
-  }
-
-  @override
-  void stopBackGroundFetchCoinsMarkets() {
-    _favoritesCoinsController.close();
-  }
-
-  @override
-  void stopBackGroundFetchFavoritesCoinsMarkets() {
-    _favoritesCoinsController.close();
+    return Future.value(Result.ok(kMarketApiModel));
   }
 }
