69947d00f6b4aefaa747c01ede175e0c06c350e8
Luis Gustavo <luisgustavodevbr@gmail.com>
2025-09-19 17:29:44 -0300

    Feat: Scaffold `FavoriteScreen` tests

---
+++ test/ui/favorites/widgets/favorite_screen_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Favorite Screen', () {
+    testWidgets('1', (tester) async {
+      // TODO: Implement test
+    });
+  });
+}
